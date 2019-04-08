bb352785bd10d61983dfdfe5906f21e2ead7c315
David Shaw <david.shaw@skybettingandgaming.com>
2019-04-08 19:01:00 +0100

    Fix import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import Count from '../components/Count';
+import Count from '../components/count';
 import store from '../components/store';
 
 
